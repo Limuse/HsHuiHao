74d07bd89926c9baaf5d1bbc044856a5abe8a023
Duei <183458978@qq.com>
2015-07-10 15:16:45 +0800

    提交

--- app/src/main/java/com/huihao/activity/GuideLoading.java
+++ app/src/main/java/com/huihao/activity/GuideLoading.java
@@ -3,38 +3,81 @@ package com.huihao.activity;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.huihao.R;
+import com.huihao.common.DepthPageTransformer;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 
-import com.huihao.R;
-import com.huihao.common.DepthPageTransformer;
-import com.leo.base.activity.LActivity;
-import com.leo.base.util.LSharePreference;
-
-public class GuideLoading extends LActivity {
+public class GuideLoading extends Activity {
 	private Button start_future;
 	private ViewPager viewPager;
 	private int[] imageIds = new int[] { R.mipmap.guide_one,
-			R.mipmap.guide_two, R.mipmap.guide_three, R.mipmap.guide_four };
+			R.mipmap.guide_two, R.mipmap.guide_three };
 	private List<ImageView> imageList = new ArrayList<ImageView>();
+	private SharedPreferences sp;
+	private Editor editor;
+	private boolean isFirstIn = true;
+	private ImageView load;
 
 	@Override
-	protected void onLCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_guide_loading);
 		InitView();
-		initData();
-		initViewPage();
-		initClick();
+		initLoad();
 	}
 
+	private void initLoad() {
+		sp = getSharedPreferences("pref", 0);
+		isFirstIn = sp.getBoolean("first_pref", true);
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					Thread.sleep(3000);
+					if (!isFirstIn) {
+						handler.sendEmptyMessage(2);
+					} else {
+						handler.sendEmptyMessage(1);
+						handler.sendEmptyMessage(3);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	private Handler handler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			if(msg.what==1){
+				load.setVisibility(View.GONE);
+			}
+			if(msg.what==2){
+				getJumpIntent(HomeMain.class);
+			}
+			if(msg.what==3){
+				initData();
+				initViewPage();
+				initClick();
+			}
+		};
+	};
+
 	private void InitView() {
+		load = (ImageView) findViewById(R.id.load);
 		start_future = (Button) findViewById(R.id.start_future);
 		start_future.setVisibility(View.GONE);
 		viewPager = (ViewPager) findViewById(R.id.viewpage);
@@ -53,21 +96,25 @@ public class GuideLoading extends LActivity {
 		viewPager.setPageTransformer(true, new DepthPageTransformer());
 		viewPager.setAdapter(new PagerAdapter() {
 
+			@Override
 			public boolean isViewFromObject(View view, Object object) {
 				return view == object;
 			}
 
+			@Override
 			public Object instantiateItem(ViewGroup container, int position) {
 				container.addView(imageList.get(position));
 				return imageList.get(position);
 			}
 
+			@Override
 			public void destroyItem(ViewGroup container, int position,
 					Object object) {
 
 				container.removeView(imageList.get(position));
 			}
 
+			@Override
 			public int getCount() {
 				return imageList.size();
 			}
@@ -75,10 +122,10 @@ public class GuideLoading extends LActivity {
 	}
 
 	private void initClick() {
-		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
 
 			public void onPageSelected(int position) {
-				if (position == 3) {
+				if (position == 2) {
 					start_future.setVisibility(View.VISIBLE);
 				} else {
 					start_future.setVisibility(View.GONE);
@@ -96,7 +143,8 @@ public class GuideLoading extends LActivity {
 	}
 
 	public void start(View v) {
-		LSharePreference.getInstance(this).setBoolean("first_pref", false);
+		editor = sp.edit();
+		editor.putBoolean("first_pref", false).commit();
 		getJumpIntent(HomeMain.class);
 	}
 
